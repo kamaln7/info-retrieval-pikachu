d9fe325b68f94f4ec9d7e9d4e43821e5199c9e61
emiljahshan <emiljahshan@192.168.1.20>
2018-06-13 20:19:48 +0300

    changes

--- IR_new/src/HandsOnDemo.java
+++ IR_new/src/HandsOnDemo.java
@@ -17,13 +17,22 @@
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -62,15 +71,24 @@ import org.apache.lucene.search.vectorhighlight.FieldQuery;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
+import org.apache.uima.internal.util.ArrayUtils;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.Iterables;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 
 
 public class HandsOnDemo {
     private static final String BODY_FIELD = "body";
+    
+    private static final String[] WH_STOP_WORDS = {"how", "what", "where", "who", "when", "which", "whom", "whose", "why", "do"};
+    
+    private static final String path = "/Users/emiljashshan/Documents/Main/Current_Courses/Information_recovery/python/corpus_answers.txt";
 
     private static final FieldType TERM_VECTOR_TYPE;
     static {
@@ -82,14 +100,11 @@ public class HandsOnDemo {
     }
 
     public static void main(String[] args) throws Exception {
-    	
-    		String path = "/Users/emiljashshan/git/IR/src/nfL6.json";
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-
+    	    		
         Gson gson = new Gson();
         
         //parse Json file
-        Data[] data = gson.fromJson(bufferedReader, Data[].class);
+        //Data[] data = gson.fromJson(bufferedReader, Data[].class);
         
         try (Directory dir = newDirectory();
         Analyzer analyzer = newAnalyzer()) 
@@ -97,14 +112,15 @@ public class HandsOnDemo {
             // Index
             try (IndexWriter writer = new IndexWriter(dir, newIndexWriterConfig(analyzer))) 
             {
-                for (int i = 0; i < data.length; i++)
-                {
-	                	for (String answer : data[i].getNbestanswers()) 
-	                	{
-	                		final Document doc = new Document();
-                        doc.add(new TextField("body", answer, Store.YES));
-                        writer.addDocument(doc);
-	            		}
+            		try (BufferedReader br = new BufferedReader(new FileReader(path)))
+            		{
+                    String answer;
+                    while ((answer = br.readLine()) != null) 
+                    {
+                    		final Document doc = new Document();
+                    		doc.add(new TextField("body", answer, Store.YES));
+                    		writer.addDocument(doc);
+                    }
                 }
             }
 
@@ -113,20 +129,48 @@ public class HandsOnDemo {
             {
                 final QueryParser qp = new QueryParser(BODY_FIELD, analyzer);
                 
-                String query = "How can I behave in a more spontaneous way?";
+                final List<String> query_words = new LinkedList<String>();
+                
+                String query = "For colIege admission, is it better to take AP classes and get Bs or easy classes and get As?";
                 
                 if(query.charAt(query.length()-1) == '?') 
                 {
                 		query = query.substring(0, query.length() - 1);
                 }
                 
-                String[] words = query.split("\\s+");
+                String[] query_words_array = query.split("\\s+");
+                
+                for (int i=0 ; i<query_words_array.length ; i++)
+                {
+                		query_words.add(query_words_array[i]);
+                }
+                
+                for (int i=0 ; i<query_words.size() ; i++)
+                {
+            			if(query_words.get(i).toLowerCase().equals("why")) {query_words.add(i,"because"); i++; 
+            																query_words.add(i,"coz"); i++; 
+            																query_words.add(i,"cuz"); i++; }
+            			if(query_words.get(i).toLowerCase().equals("when")) {query_words.add(i, "at"); i++; }
+            			if(query_words.get(i).toLowerCase().equals("which")) {query_words.add(i, "one"); i++; }
+                }
+                
+                for (int i=0 ; i<query_words.size() ; i++)
+                {
+                		for(int j=0 ; j< WH_STOP_WORDS.length ; j++)
+                		{
+                			if(query_words.get(i).toLowerCase().equals(WH_STOP_WORDS[j]))
+                			{
+                				query_words.remove(i);
+                			}
+                		}
+                }
+                
                 StringBuilder tmp_new_query = new StringBuilder();
                 
-                for(int i=0 ; i< words.length ; i++)
+                for(int i=0 ; i< query_words.size() ; i++)
                 {
                 		Runtime rt = Runtime.getRuntime();
-                		Process pr = rt.exec("/usr/local/python3 /Users/emiljashshan/Documents/main/Current_Courses/Information_recovery/python/syn.py " + words[i]);
+                		Process pr = rt.exec("/usr/local/python3 /Users/emiljashshan/Documents/main/Current_Courses/Information_recovery/python/syn.py " + query_words.get(i).toLowerCase());
                 		pr.waitFor();
                 		
                 		BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
@@ -139,11 +183,10 @@ public class HandsOnDemo {
                         	word_add = line;
                     	}
                 		
-                		tmp_new_query.append("body: " + words[i] + " ");
+                		tmp_new_query.append("body: " + query_words.get(i) + " ");
                 		if(word_add != "")
                 		{
                 			StringBuilder add_query = new StringBuilder();
-                			//add_query.append(q);
                 			add_query.append(" body:" + word_add);
                 			tmp_new_query.append("body: " + word_add + " ");
                 		}
@@ -162,7 +205,7 @@ public class HandsOnDemo {
                 final IndexSearcher searcher = new IndexSearcher(reader);
                 //searcher.setSimilarity(new ClassicSimilarity());
                 searcher.setSimilarity(new BM25Similarity());
-                final TopDocs td = searcher.search(q, 25);
+                final TopDocs td = searcher.search(q, 300);
 
                 ArrayList<answer> answer_list = new ArrayList<answer>();
                 answers big_answer = new answers();
@@ -170,7 +213,11 @@ public class HandsOnDemo {
                 final FastVectorHighlighter highlighter = new FastVectorHighlighter();
                 final FieldQuery fieldQuery = highlighter.getFieldQuery(q, reader);
                 
-                int count=0;
+                int count = 0;
+                int count_300 = 0;
+                
+                PrintStream out = new PrintStream(new FileOutputStream("/Users/emiljashshan/Documents/Main/Current_Courses/Information_recovery/python/top_300_answers.txt"));
+                
                 for (final ScoreDoc sd : td.scoreDocs) 
                 {
                 		answer answer1 = new answer();
@@ -187,22 +234,49 @@ public class HandsOnDemo {
                     		big_answer.setAnswers(answer_list);
                     		gson = new GsonBuilder().setPrettyPrinting().create();
                     		String strJson = gson.toJson(big_answer);
-                    		System.out.println(String.format("doc=%d, score=%.4f, text=%s snippet=%s", sd.doc, sd.score,
-                                    doc.get(BODY_FIELD), Arrays.stream(snippets).collect(Collectors.joining(" "))));
-                    		break;
+                    		//System.out.println(String.format("doc=%d, score=%.4f, text=%s snippet=%s", sd.doc, sd.score,
+                                    //doc.get(BODY_FIELD), Arrays.stream(snippets).collect(Collectors.joining(" "))));
+                    		
+                    		System.out.println(strJson);
+                    		//break;
                     }
                     
                     answer_list.add(answer1);
                     count++;
-                    System.out.println(String.format("doc=%d, score=%.4f, text=%s snippet=%s", sd.doc, sd.score,
-                            doc.get(BODY_FIELD), Arrays.stream(snippets).collect(Collectors.joining(" "))));
-                }
+                    
+                    if(count_300 < 300)
+                    {
+                    		out.println(doc.get(BODY_FIELD));
+                    		count_300++;
+                    		
+                    		if(count_300 < 50)
+                    		{
+		                    	System.out.println(String.format("doc=%d, score=%.4f, text=%s snippet=%s", sd.doc, sd.score,
+		                                doc.get(BODY_FIELD), Arrays.stream(snippets).collect(Collectors.joining(" "))));
+                    		}
+                    }
+                } // end of top-docs
+                
+                //String[] most_freq_words = countMostFreqWord();
             }
         }
     }
+    
+    /*private static String[] countMostFreqWord() throws FileNotFoundException, IOException 
+    {
+    		
+	    	try (BufferedReader br = new BufferedReader(new FileReader(path)))
+			{
+	        String answer;
+	        while ((answer = br.readLine()) != null) 
+	        {
+	        		
+	        }
+	    }
+    }*/
 
-    private static Directory newDirectory() throws IOException {
-        // return new RAMDirectory();
+    private static Directory newDirectory() throws IOException 
+    {
         return FSDirectory.open(new File("d:/tmp/ir-class/demo").toPath());
     }
     
@@ -221,7 +295,6 @@ public class HandsOnDemo {
         return new IndexWriterConfig(analyzer)
                 .setOpenMode(OpenMode.CREATE)
                 .setCodec(new SimpleTextCodec())
-                // .setUseCompoundFile(false)
                 .setCommitOnClose(true);
     }
 }
